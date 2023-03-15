2f70112dedcd2a7721a69b9dad301731a9d96b2f
wen0621 <85426632+wen0621@users.noreply.github.com>
2023-03-16 01:08:32 +0800

    from狗哥

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,7 +1,19 @@
-﻿#include <iostream>
-
+﻿//db
+#include <iostream>
+#include <string>
 using namespace std;
-
-int main() {
-
+int main()
+{
+    int a = 0;
+    string str;
+    while (cin >> a) {
+        str = "";
+        if (a < 0) a += 256;
+        for (int i = 0; i < 8; i++) {
+            str = str.insert(0, to_string(a % 2));
+            a /= 2;
+        }
+        cout << str << endl;
+    }
+    system("pause");
 }
