2bc94d73ac8ad5af9064d4a89ad44c3256fe942a
wen0621 <85426632+wen0621@users.noreply.github.com>
2023-03-16 01:05:12 +0800

    from狗哥

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,7 +1,19 @@
-﻿#include <iostream>
-
+﻿//db
+#include <iostream>
+#include <math.h>
+#define R 200
 using namespace std;
 
-int main() {
-
+int main()
+{
+    int x = 0, y = 0;
+    while (cin >> x >> y) {
+        if ((pow(x, 2) + pow(y, 2) <= pow(R / 2, 2))) {
+            cout << "inside\n";
+        }
+        else {
+            cout << "outside\n";
+        }
+    }
+    return 0;
 }
