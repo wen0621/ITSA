2b9418d3d2a33558ee2d35a351ec919d538384e4
wen0621 <85426632+wen0621@users.noreply.github.com>
2023-03-16 01:33:21 +0800

    self

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,7 +1,18 @@
 ﻿#include <iostream>
-
 using namespace std;
-
+int gcd(int m, int n) {
+    int t = 1;
+    while (t != 0) {
+        t = m % n;
+        m = n;
+        n = t;
+    }
+    return m;
+}
 int main() {
-
+    int x, y;
+    cin >> x;
+    cin >> y;
+    cout << gcd(x, y) << endl;
+    return 0;
 }
