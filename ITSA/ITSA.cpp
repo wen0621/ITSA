24c9964649ad8efe259524438f8993130ca144c3
wen0621 <85426632+wen0621@users.noreply.github.com>
2023-03-16 01:00:20 +0800

    main

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,7 +1,12 @@
 ﻿#include <iostream>
-
+#include <iomanip>
 using namespace std;
 
-int main() {
-
+int main()
+{
+	int ml;
+	double km;
+	cin >> ml;
+	km = double(ml * 1.6);
+	cout << fixed << setprecision(1) << km << endl;
 }
