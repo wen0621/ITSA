29aeff438b6e194e36a7dffc9a2bc2dc7f606492
wen0621 <85426632+wen0621@users.noreply.github.com>
2023-03-16 01:23:25 +0800

    from狗哥

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,1 +1,16 @@
 ﻿//db
+#include <iostream>
+using namespace std;
+int main()
+{
+    int input = 0, sum = 0;
+    while (cin >> input)
+    {
+        for (int i = 1; i <= input; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+            }
+        }
+        cout << sum << endl;
+    }
+}
