1d19fc32cf70c02a87648088aa61a8a7f4c5b6f3
wen0621 <85426632+wen0621@users.noreply.github.com>
2023-03-16 01:20:21 +0800

    from狗哥

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,8 +1,23 @@
-﻿//db
+//db
 #include <iostream>
-
 using namespace std;
+int main()
+{
 
-int main() {
-
+    int input = 0;
+    while (cin >> input)
+    {
+        int check = 0;
+        for (int i = 2; i <= input - 1; i++) {
+            if (input % i == 0) {
+                check = +1;
+            }
+        }
+        if (check == 0) {
+            cout << "YES\n";
+        }
+        else {
+            cout << "NO\n";
+        }
+    }
 }
