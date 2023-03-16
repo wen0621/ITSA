2810d4de070b861dada489474662c6fc761f0fe5
wen0621 <85426632+wen0621@users.noreply.github.com>
2023-03-16 19:03:01 +0800

    self

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,7 +1,15 @@
 ﻿#include <iostream>
-
 using namespace std;
 
+int funtion(int num) {
+	if ((num == 0) || (num == 1))
+		return num + 1;
+	else
+		return funtion(num - 1) + funtion(num / 2);
+}
 int main() {
-
+	int num;
+	cin >> num;
+	cout << funtion(num) << endl;
+	return 0;
 }
