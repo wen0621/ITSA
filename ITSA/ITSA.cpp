5318d8763d2c66043ee8dd0f06c942d2d2d641d7
wen0621 <85426632+wen0621@users.noreply.github.com>
2023-03-16 17:58:37 +0800

    test

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,7 +1,21 @@
 ﻿#include <iostream>
-
 using namespace std;
 
 int main() {
-
+    int row, column;
+    while (cin >> column >> row) {
+        int data[column][row];
+        for (int i = 0; i < column; i++) {
+            for (int j = 0; j < row; j++) {
+                cin >> data[i][j];
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                cout << data[j][i] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
 }
