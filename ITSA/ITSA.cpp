0546a3bbdb80ba33218d53c9459a67858c2a6c7d
wen0621 <85426632+wen0621@users.noreply.github.com>
2023-03-19 07:20:31 +0800

    hmm

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,10 +1,18 @@
 ﻿#include <iostream>
 using namespace std;
 
+void sort(string input[]) {
+	int len = input.length();
+	cout << len;
+}
+
 int main() {
 	int num;
+	char* input;
 	cin >> num;
 	for (int i = 0; i < num; i++) {
-
+		cin >> input;
+		sort(strtok(input, ' '));
 	}
+	return 0;
 }
