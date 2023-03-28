6ab16b041475a5f1bfd6d8839d95a445e833de4e
wen0621 <85426632+wen0621@users.noreply.github.com>
2023-03-28 21:34:41 +0800

    0328

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -5,15 +5,12 @@ int main() {
     int row, column;
     cin >> column >> row;
     int data[10][10];
-    for (int i = 0; i < column; i++) {
-        for (int j = 0; j < row; j++) {
+    for (int i = 0; i < column; i++) 
+        for (int j = 0; j < row; j++) 
             cin >> data[i][j];
-        }
-    }
     for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++) {
+        for (int j = 0; j < column; j++) 
             cout << data[j][i] << " ";
-        }
         cout << "\b" << endl;
     }
     return 0;
