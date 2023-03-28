66c5a1b580a248924938d4485207014b1fd2067a
wen0621 <85426632+wen0621@users.noreply.github.com>
2023-03-28 21:44:31 +0800

    tab

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -7,26 +7,26 @@ int main()
     int a = 0;
     cin >> a;
     switch (a) {
-    case 12:
-    case 1:
-    case 2:
-        cout << "Winter\n";
-        break;
-    case 3:
-    case 4:
-    case 5:
-        cout << "Spring\n";
-        break;
-    case 6:
-    case 7:
-    case 8:
-        cout << "Summer\n";
-        break;
-    case 9:
-    case 10:
-    case 11:
-        cout << "Autumn\n";
-        break;
+        case 12:
+        case 1:
+        case 2:
+            cout << "Winter\n";
+            break;
+        case 3:
+        case 4:
+        case 5:
+            cout << "Spring\n";
+            break;
+        case 6:
+        case 7:
+        case 8:
+            cout << "Summer\n";
+            break;
+        case 9:
+        case 10:
+        case 11:
+            cout << "Autumn\n";
+            break;
     }
     return 0;
 }
